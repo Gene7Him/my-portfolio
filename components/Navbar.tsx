6a713173a80f49d26a2405337d95b5c78946350a
Gene7Him <155666233+Gene7Him@users.noreply.github.com>
2025-07-06 18:41:24 -0700

    added resume download and removed blog page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,6 @@ export default function Navbar() {
       <ul className="flex space-x-4 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-4 leading-relaxed tracking-tight">
         <li><a href="/" className="hover:underline">Home</a></li>
         <li><a href="/projects" className="hover:underline">Projects</a></li>
-        <li><a href="/blog" className="hover:underline">Blog</a></li>
         <li><a href="/about" className="hover:underline">About</a></li>
         <li><a href="/resume" className="hover:underline">Resume</a></li>
       </ul>
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -6,7 +6,7 @@ export default function Resume() {
       <div className="min-h-screen flex flex-col">
       <main className="flex-1 flex flex-col justify-center items-center text-center p-10">
         <h1 className="text-4xl font-extrabold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-4 drop-shadow-lg">Resume</h1>
-        <a href="/resume.pdf" target="_blank" className="text-blue-500">Download my resume</a>
+        <a href="/resume.pdf" target="_blank" className="text-blue-500">Download PDF</a>
       </main>
       </div>
     </>
