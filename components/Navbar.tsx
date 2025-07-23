05e039386078d5c85761e258c50935fbc7b07a1b
Gene7Him <155666233+Gene7Him@users.noreply.github.com>
2025-07-23 11:03:31 -0700

    mobile navigation hamburger layout fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
       className="sticky top-0 z-50 bg-gradient-light dark:bg-gradient-dark shadow-md backdrop-blur-md"
     >
     <div className="max-w-6xl mx-auto flex justify-between items-center px-6 py-4">
-        <div className="text-xl font-bold text-blue-600 dark:text-purple-400">Eugene</div>
+        
         
         {/* Hamburger Toggle */}
         <div className="md:hidden">
@@ -30,7 +30,7 @@ export default function Navbar() {
           </button>
         </div>
 
-      <ul className="flex space-x-4 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-4 leading-relaxed tracking-tight">
+      <ul className="flex space-x-4 font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-4 leading-relaxed tracking-tight">
         <li><a href="/" className="hover:underline">Home</a></li>
         <li><a href="/projects" className="hover:underline">Projects</a></li>
         <li><a href="/about" className="hover:underline">About</a></li>
@@ -63,7 +63,21 @@ export default function Navbar() {
           <li><Link href="/projects">Projects</Link></li>
           <li><Link href="/about">About</Link></li>
           <li><Link href="/resume">Resume</Link></li>
-          <li><a href="https://foundbyfaison.tech" target="_blank">FoundByFaison</a></li>
+          <li>
+          <a
+            href="https://www.foundbyfaison.tech"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-2 hover:underline"
+          >
+            <img
+              src="/logomain.png"
+              alt="FoundByFaison Logo"
+              className="w-6 h-6 transition-transform duration-300 hover:scale-110"
+            />
+            <span className="hover:no-underline">FoundByFaison</span>
+          </a>
+        </li>
           <ThemeToggle />
         </ul>
       )}
